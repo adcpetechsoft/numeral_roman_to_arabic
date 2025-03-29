695fb8c3751a3e26fa8686ed013f3e5685e7fe06
amdg <adcpetechsoft@hotmail.com>
2025-03-29 19:04:50 +0800

    finish

--- convert.cpp
+++ convert.cpp
@@ -1,3 +1,14 @@
+/*
+    Roman to  Arabic program
+    Convert Roman to HinduArabic Numeral
+    Submitted to  https://code.golf/roman-to-arabic#cpp
+
+    Created by: amd
+*/
+
+
+// ---------------------------------
+// Preprocessor Declaration
 #define STR_SIZE 50
 
 #define P_OK 0
@@ -8,17 +19,28 @@
 #include <string.h>
 #include <ctype.h>
 
+// ---------------------------------
+// Global Varibles
+
 char num_roman[STR_SIZE];
 int num_arabic;
 
 int str_loc;
 
+// *********************************
+// convert_thousnad method
 int convert_thousnad()
 {
+    // --------------------------
+    // variable Declaration
     int res;
 
     int i;
 
+    // --------------------------
+    // Actual Function
+
+    // Scan for 1000 to 3000 values
     if(num_roman[str_loc]=='M' && num_roman[str_loc] != '\0')
     {
         // 1000 to 3000
@@ -30,7 +52,6 @@ int convert_thousnad()
         };
 
     };
-    
 
     res=P_OK;
 
@@ -38,12 +59,20 @@ int convert_thousnad()
 
 };
 
+
+// *********************************
+// convert_hundres method
 int convert_hundres()
 {
+    // ----------------------------
+    // variable declaration
     int res;
 
     int i;
 
+    // ----------------------------
+    // Actual Function
+    
     // for 500 to 800
     if(num_roman[str_loc]=='D')
     {
@@ -101,27 +130,34 @@ int convert_hundres()
 };
 
 
-// tens
+// *********************************
+// convert_tens method
 int convert_tens()
 {
+    // ---------------------------
+    // Variable  declaration
     int res;
 
     int i;
 
-       // for 50 to 80
-       if(num_roman[str_loc]=='L' && num_roman[str_loc] != '\0')
-       {
-           num_arabic+=50;
-           str_loc++;
-           i=0;
-           while(num_roman[str_loc]=='X' && i<3 && num_roman[str_loc] != '\0')
-           {
-               num_arabic+=10;
-               str_loc++;
-               i++;
-           };
+
+    // ----------------------------
+    // Actual funciton
+
+    // for 50 to 80
+    if(num_roman[str_loc]=='L' && num_roman[str_loc] != '\0')
+    {
+        num_arabic+=50;
+        str_loc++;
+        i=0;
+        while(num_roman[str_loc]=='X' && i<3 && num_roman[str_loc] != '\0')
+        {
+            num_arabic+=10;
+            str_loc++;
+            i++;
+        };
    
-       };
+    };
 
     // for 90, 40 and 10-30
     if(num_roman[str_loc]=='X' && num_roman[str_loc]!='\0')
@@ -166,13 +202,21 @@ int convert_tens()
 
 };
 
-// ones
+// *********************************
+// convert_ones method
 int convert_ones()
 {
+    
+    // -----------------------------
+    // Variable  Declaration
     int res;
 
     int i;
 
+
+    // -----------------------------
+    // Actual Funciton
+    
     // for 5 to 8
     if(num_roman[str_loc]=='V' && num_roman[str_loc] != '\0')
     {
@@ -224,18 +268,22 @@ int convert_ones()
 
     };
 
-    
-
     res=P_OK;
 
     return res;
 
 };
 
-
+// *********************************
+// convert method
 int convert()
 {
+    // ----------------------------
+    // Variable Declaration
     int res;
+
+    // ---------------------------
+    // Actual Function
 
     str_loc=0;
     num_arabic=0;
@@ -251,24 +299,41 @@ int convert()
 
 }
 
+// *********************************
+// ---------------------------------
+// Progran  Entry Point
+// ---------------------------------
+// *********************************
 int main(int argc, char *argv[])
 {
+    // ----------------------------
+    // Variable  Declaration
     int x;
 
     char str01[STR_SIZE];
 
+    
+    // ----------------------------
+    // Actual function
+
+    // initialized var for loping test
     x=1;
 
+    // check the ARG present
     if(argc>1)
     {
+        // scanning all the varibles to  convert
         while(x<argc)
         {
+            // call the convert function then display
             snprintf(num_roman, sizeof(num_roman), "%s", argv[x]);
             if( convert() == P_OK )
             {
+                // Display if Sucessfull
                 printf("%i\n", num_arabic);
             }else
             {
+                // Print if Error
                 printf("ERROR: Improper Roman Numeral Input...\n");
             };
 
@@ -279,11 +344,10 @@ int main(int argc, char *argv[])
 
     }else
     {
+        // Print if no ARG
         printf("ERROR: No ARG input... Input Roman Numeral to Cenvert as ARG...\n");
     };
 
     return 0;
 
 };
-
-
